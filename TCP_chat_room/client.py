0d6aff39748e5d0795a8b627c220503cdd4c20ae
SANS002 <sans9361639086@gmail.com>
2025-06-23 20:40:54 +0530

    TCP CHAT ROOM

---
+++ TCP_chat_room/client.py
@@ -0,0 +1,38 @@
+import socket
+import threading
+
+nickname = input("Choose a nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55555))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An error occurred. Disconnecting...")
+            client.close()
+            break
+
+def write():
+    while True:
+        try:
+            msg = input('')
+            if msg.strip() != "":
+                message = f"{nickname}: {msg}"
+                client.send(message.encode('ascii'))
+        except:
+            print("An error occurred while sending.")
+            client.close()
+            break
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
---
+++ TCP_chat_room/server.py
@@ -0,0 +1,59 @@
+import threading
+import socket
+
+host = '127.0.0.1'
+port = 55555
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        try:
+            client.send(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            broadcast(f"{nickname} has been removed due to an error.".encode('ascii'))
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            broadcast(f"{nickname} left the chat.".encode('ascii'))
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
+
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname is {nickname}")
+        broadcast(f"{nickname} joined the chat!".encode('ascii'))
+        client.send("You are connected to the server!".encode('ascii'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print("Server is listening...")
+receive()
